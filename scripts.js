3f7aa9f16e2b4f573294e36c5fcde7a0c34963be
Aaron Bushnell <aaron.bushnell@gmail.com>
2020-05-05 10:54:59 -0400

    Run events on Turbolink navigation changes; Add list details to todo items; Fix client visibility bubble placement

--- scripts.js
+++ scripts.js
@@ -1,8 +1,14 @@
-(() => {
-  // Add a class to the body to style in a more gentle way
+/**
+ * Add a class to the body to style in a more gentle way
+ */
+function addBodyClass() {
   document.querySelector("body").classList.add("basecamp-enhancer");
+}
 
-  // Swap the generic Basecamp logo with the company name
+/**
+ * Swap the generic Basecamp logo with the company name
+ */
+function addCompanyDetails() {
   const BCName = document.querySelector("meta[name='current-account-name']")
     .content;
 
@@ -10,4 +16,64 @@
   node.innerText = BCName;
   node.classList.add("basecamp-enhancer__logo-label");
   document.querySelector(".nav__link--accounts").appendChild(node);
-})();
+}
+
+/**
+ * Attempt to add the todo list details into a todo item
+ */
+function addListDetailsToTodoItem() {
+  // If this is a todo, let's try to pull in the todolist details
+  if (/\/\d+\/buckets\/\d+\/todos\/\d+/.test(window.location.href)) {
+    // Do we have access to a todolist URL?
+    const $todoLink = document.querySelector(
+      ".recording-breadcrumb__link:last-of-type [data-target='breadcrumbs.link']"
+    );
+
+    if ($todoLink && /todolists/.test($todoLink.href))
+      fetch(
+        document.querySelector(
+          ".recording-breadcrumb__link:last-of-type [data-target='breadcrumbs.link']"
+        ).href
+      )
+        .then((response) => response.text())
+        .then((html) => {
+          const doc = new DOMParser().parseFromString(html, "text/html");
+          const listTitle = doc.querySelector("[data-bridge-alt]").innerText;
+          const listDesc = doc.querySelector(".todolist__description")
+            .innerText;
+
+          document.querySelector(".todo-perma__details").insertAdjacentHTML(
+            "beforebegin",
+            `
+        <div class="todolist-details">
+          <h4>Todo list</h4>
+          <h2>${listTitle}</h2>
+          <p>${listDesc}</p>
+        </div>
+      `
+          );
+        })
+        .catch((err) =>
+          console.warn(
+            "Something went wrong grabbing the todolist description.",
+            err
+          )
+        );
+  }
+}
+
+/**
+ * Fire events when navigating between pages via Turbolinks
+ */
+document.addEventListener("turbolinks:load", () => {
+  addBodyClass();
+  addCompanyDetails();
+  addListDetailsToTodoItem();
+});
+
+/**
+ * Run the events when the document runs
+ */
+addBodyClass();
+addCompanyDetails();
+addListDetailsToTodoItem();
